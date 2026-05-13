f1d0b43779ea500c8c5f5805b2de85f0652ab73d
Ana Estrada <anaestrada@Anas-MacBook-Pro.local>
2026-05-13 10:44:40 -0600

    added confirmation message and details info

--- src/app/components/booking-detail/booking-detail.ts
+++ src/app/components/booking-detail/booking-detail.ts
@@ -1,4 +1,4 @@
-import { Component, input, signal } from '@angular/core';
+import { Component, input, output, signal, effect } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Booking } from '../../models/booking.model';
 
@@ -11,9 +11,37 @@ import { Booking } from '../../models/booking.model';
 })
 export class BookingDetailComponent {
   booking = input<Booking | null>(null);
+  bookingClosed = output<void>();
   reserved = signal(false);
+  reserveError = signal(false);
+  toastVisible = signal(false);
+
+  constructor() {
+    effect(() => {
+      if (this.booking()) {
+        this.reserved.set(false);
+        this.reserveError.set(false);
+        this.toastVisible.set(false);
+      }
+    });
+  }
 
   reserve() {
-    this.reserved.set(true);
+    if (this.booking()!.availableSpots === 0) {
+      this.reserveError.set(true);
+    } else {
+      this.reserved.set(true);
+      this.reserveError.set(false);
+    }
+    this.showToast();
+  }
+
+  private showToast() {
+    this.toastVisible.set(true);
+    setTimeout(() => this.toastVisible.set(false), 3000);
+  }
+
+  close() {
+    this.bookingClosed.emit();
   }
 }
